b1cb273acf748f580e2e1d85e60e4a67260eef1f
luttinenjoni <luttinenjoni@gmail.com>
2025-02-19 11:01:35 +0200

    done

--- streamlit.py
+++ streamlit.py
@@ -1,12 +1,20 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
  
-st.write("""
-# Survived vs Surviwont
-""")
+col1, col2 = st.columns([3, 8])
+
+with col1:
+    custom_title = st.text_input("Enter a custom title:", "Survivors and Deaths")
+    st.write(f"## {custom_title}")
  
 data = pd.read_excel(".\DataAnalysisAssignment4\data\Titanic Data.xlsx")
 
 survival_counts = data["Survived"].value_counts()
+survival_counts.index = ["Died", "Survived"] 
 
-st.bar_chart(survival_counts)
+fig, ax = plt.subplots()
+ax.bar(survival_counts.index, survival_counts.values)
+
+with col2:
+    st.pyplot(fig)
